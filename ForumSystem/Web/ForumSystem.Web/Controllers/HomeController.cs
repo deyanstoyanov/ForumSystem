8e58e8d82a0d52a26f3b8d0772ec10e1cdddfdcc
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-17 20:49:40 +0300

    Cleanup ForumSystem.Web project by uninstalling NuGet packages

--- ForumSystem/Web/ForumSystem.Web/Controllers/HomeController.cs
+++ ForumSystem/Web/ForumSystem.Web/Controllers/HomeController.cs
@@ -8,19 +8,5 @@
         {
             return this.View();
         }
-
-        public ActionResult About()
-        {
-            this.ViewBag.Message = "Your application description page.";
-
-            return this.View();
-        }
-
-        public ActionResult Contact()
-        {
-            this.ViewBag.Message = "Your contact page.";
-
-            return this.View();
-        }
     }
 }
