bd00743926b7074365e08b33e74eb3c5558981ab
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-17 16:44:11 +0300

    Created interfaces for repositories (IRepository, IDeletableEntityRepository)

---
+++ ForumSystem/Data/ForumSystem.Data.Common/Models/IDeletableEntityRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace ForumSystem.Data.Common.Models
+{
+    using System.Linq;
+
+    public interface IDeletableEntityRepository<T> : IRepository<T>
+        where T : class
+    {
+        IQueryable<T> AllWithDeleted();
+
+        void ActualDelete(T entity);
+    }
+}
---
+++ ForumSystem/Data/ForumSystem.Data.Common/Models/IRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace ForumSystem.Data.Common.Models
+{
+    using System;
+    using System.Linq;
+
+    public interface IRepository<T> : IDisposable
+        where T : class
+    {
+        IQueryable<T> All();
+
+        T GetById(object id);
+
+        T Add(T entity);
+
+        T Update(T entity);
+
+        T Delete(object id);
+
+        void Delete(T entity);
+
+        int SaveChanges();
+    }
+}
