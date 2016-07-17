99ce5375aa53420aa2aaca3e95007e32b953e554
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-17 22:41:48 +0300

    Changed validation logic for passwords

--- ForumSystem/Web/ForumSystem.Web/App_Start/ApplicationUserManager.cs
+++ ForumSystem/Web/ForumSystem.Web/App_Start/ApplicationUserManager.cs
@@ -36,10 +36,10 @@
             manager.PasswordValidator = new PasswordValidator
                                             {
                                                 RequiredLength = 6, 
-                                                RequireNonLetterOrDigit = true, 
-                                                RequireDigit = true, 
-                                                RequireLowercase = true, 
-                                                RequireUppercase = true
+                                                RequireNonLetterOrDigit = false, 
+                                                RequireDigit = false, 
+                                                RequireLowercase = false, 
+                                                RequireUppercase = false
                                             };
 
             // Configure user lockout defaults
