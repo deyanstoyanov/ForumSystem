4443092512f2995998100726378775910c3b90e4
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-17 16:45:28 +0300

    Implemented repository interfaces

---
+++ ForumSystem/Data/ForumSystem.Data/Repositories/DeletableEntityRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace ForumSystem.Data.Repositories
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    using System.Linq;
+
+    using ForumSystem.Data.Common.Models;
+
+    internal class DeletableEntityRepository<T> : GenericRepository<T>, IDeletableEntityRepository<T>
+        where T : class, IDeletableEntity
+    {
+        public DeletableEntityRepository(DbContext dbContext)
+            : base(dbContext)
+        {
+        }
+
+        public override IQueryable<T> All()
+        {
+            return base.All().Where(x => !x.IsDeleted);
+        }
+
+        public IQueryable<T> AllWithDeleted()
+        {
+            return base.All();
+        }
+
+        public override void Delete(T entity)
+        {
+            entity.IsDeleted = true;
+            entity.DeletedOn = DateTime.Now;
+
+            DbEntityEntry entry = this.Context.Entry(entity);
+            entry.State = EntityState.Modified;
+        }
+
+        public void ActualDelete(T entity)
+        {
+            base.Delete(entity);
+        }
+    }
+}
---
+++ ForumSystem/Data/ForumSystem.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,75 @@
+﻿namespace ForumSystem.Data.Repositories
+{
+    using System.Data.Entity;
+    using System.Linq;
+
+    using ForumSystem.Data.Common.Models;
+
+    internal class GenericRepository<T> : IRepository<T>
+        where T : class
+    {
+        public GenericRepository(DbContext dbContext)
+        {
+            this.Context = dbContext;
+            this.EntitySet = dbContext.Set<T>();
+        }
+
+        protected DbContext Context { get; set; }
+
+        public IDbSet<T> EntitySet { get; }
+
+        public virtual IQueryable<T> All()
+        {
+            return this.EntitySet;
+        }
+
+        public virtual T GetById(object id)
+        {
+            return this.EntitySet.Find(id);
+        }
+
+        public virtual T Add(T entity)
+        {
+            return this.ChangeState(entity, EntityState.Added);
+        }
+
+        public virtual T Update(T entity)
+        {
+            return this.ChangeState(entity, EntityState.Modified);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            this.ChangeState(entity, EntityState.Deleted);
+        }
+
+        public virtual T Delete(object id)
+        {
+            var entity = this.GetById(id);
+            this.Delete(entity);
+            return entity;
+        }
+
+        public int SaveChanges()
+        {
+            return this.Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            this.Context.Dispose();
+        }
+
+        private T ChangeState(T entity, EntityState state)
+        {
+            var entry = this.Context.Entry(entity);
+            if (entry.State == EntityState.Detached)
+            {
+                this.EntitySet.Attach(entity);
+            }
+
+            entry.State = state;
+            return entity;
+        }
+    }
+}
