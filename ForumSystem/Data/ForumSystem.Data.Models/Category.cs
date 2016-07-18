5f4bc5ce50c63fb61a090fbf2af21d9db9abd763
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-18 15:55:15 +0300

    Created CategoryConciseViewModel

--- ForumSystem/Data/ForumSystem.Data.Models/Category.cs
+++ ForumSystem/Data/ForumSystem.Data.Models/Category.cs
@@ -20,6 +20,9 @@
         [StringLength(200, MinimumLength = 3, ErrorMessage = "The {0} must be between {1} and {2} symbols.")]
         public string Title { get; set; }
 
+        [MaxLength(200)]
+        public string Description { get; set; }
+
         public int SectionId { get; set; }
 
         public virtual Section Section { get; set; }
---
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/Category/CategoryConciseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ForumSystem.Web.ViewModels.Category
+{
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+
+    public class CategoryConciseViewModel : IMapFrom<Category>
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
