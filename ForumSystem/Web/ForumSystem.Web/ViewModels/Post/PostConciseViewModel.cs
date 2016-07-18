cbbcf2ccd07bd98b18ba32de9b49b1b75c306252
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-18 18:35:24 +0300

    Created PostConciseViewModel

---
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/Post/PostConciseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ForumSystem.Web.ViewModels.Post
+{
+    using AutoMapper;
+
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+
+    public class PostConciseViewModel : IMapFrom<Post>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<Post, PostConciseViewModel>()
+                .ForMember(p => p.Author, config => config.MapFrom(p => p.Author.UserName));
+        }
+    }
+}
