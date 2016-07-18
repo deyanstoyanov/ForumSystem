8f08c1e52b806fea1ec4c9c8a06234caa80ab7c7
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-18 15:57:16 +0300

    Created SectionViewModel with custom mapping

---
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/Section/SectionViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace ForumSystem.Web.ViewModels.Section
+{
+    using System.Collections.Generic;
+
+    using AutoMapper;
+
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+    using ForumSystem.Web.ViewModels.Category;
+
+    public class SectionViewModel : IMapFrom<Section>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public IEnumerable<CategoryConciseViewModel> Categories { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<Section, SectionViewModel>()
+                .ForMember(s => s.Categories, config => config.MapFrom(s => s.Categories));
+        }
+    }
+}
