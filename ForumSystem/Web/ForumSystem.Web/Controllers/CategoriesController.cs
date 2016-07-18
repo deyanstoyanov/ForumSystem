5aafe561f58ac4967eaaac1d988403c9549359f7
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-18 18:23:54 +0300

    Created CategoriesController with sample implementation of details page

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ForumSystem.Data;
+using ForumSystem.Data.Models;
+
+namespace ForumSystem.Web.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+
+    public class CategoriesController : BaseController
+    {
+        public CategoriesController(IForumSystemData data)
+         : base(data)
+        {
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var category = this.Data.Categories.GetById(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(category);
+        }
+    }
+}
