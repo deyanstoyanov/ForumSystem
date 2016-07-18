5525b93b4ce38aceb4a0efdfe28ac63a2ab3b2ee
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-18 15:53:35 +0300

    Integrated AutoMapper with ForumSystem.Web

--- ForumSystem/Web/ForumSystem.Web/Global.asax.cs
+++ ForumSystem/Web/ForumSystem.Web/Global.asax.cs
@@ -1,19 +1,30 @@
 ﻿namespace ForumSystem.Web
 {
+    using System.Data.Entity;
+    using System.Reflection;
     using System.Web;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
 
+    using ForumSystem.Data;
+    using ForumSystem.Data.Migrations;
+    using ForumSystem.Web.Infrastructure.Mapping;
+
     public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+
             AutofacConfig.RegisterAutofac();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var autoMapperConfig = new AutoMapperConfig(Assembly.GetExecutingAssembly());
+            autoMapperConfig.Execute();
         }
     }
 }
