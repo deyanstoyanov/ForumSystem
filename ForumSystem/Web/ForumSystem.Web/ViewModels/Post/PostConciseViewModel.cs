720e6c2d3a76ad564b4baa9c25665ea1852acf4c
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-18 18:50:30 +0300

    Added Category and AnswersCount property for PostConciseViewModel

--- ForumSystem/Web/ForumSystem.Web/ViewModels/Post/PostConciseViewModel.cs
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/Post/PostConciseViewModel.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ForumSystem.Web.ViewModels.Post
+﻿namespace ForumSystem.Web.ViewModels.Post
 {
+    using System;
+
     using AutoMapper;
 
     using ForumSystem.Data.Models;
@@ -16,16 +13,22 @@ namespace ForumSystem.Web.ViewModels.Post
 
         public string Title { get; set; }
 
-        public string Content { get; set; }
-
         public string Author { get; set; }
 
         public DateTime CreatedOn { get; set; }
+
+        public string Category { get; set; }
+
+        public int AnswersCount { get; set; }
 
         public void CreateMappings(IMapperConfiguration configuration)
         {
             configuration.CreateMap<Post, PostConciseViewModel>()
                 .ForMember(p => p.Author, config => config.MapFrom(p => p.Author.UserName));
+            configuration.CreateMap<Post, PostConciseViewModel>()
+                .ForMember(p => p.Category, config => config.MapFrom(p => p.Category.Title));
+            configuration.CreateMap<Post, PostConciseViewModel>()
+                .ForMember(p => p.AnswersCount, config => config.MapFrom(p => p.Answers.Count));
         }
     }
 }
