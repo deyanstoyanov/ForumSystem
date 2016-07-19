70ff54e8a5c7b105d41a94f53fea0efd74a0ee73
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-19 10:54:37 +0300

    Implemented creating new post by authenticated user

--- ForumSystem/Web/ForumSystem.Web/Controllers/PostsController.cs
+++ ForumSystem/Web/ForumSystem.Web/Controllers/PostsController.cs
@@ -6,8 +6,12 @@
 
     using AutoMapper.QueryableExtensions;
 
+    using ForumSystem.Data.Models;
     using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.InputModels.Posts;
     using ForumSystem.Web.ViewModels.Post;
+
+    using Microsoft.AspNet.Identity;
 
     public class PostsController : BaseController
     {
@@ -36,6 +40,51 @@
                 .FirstOrDefault();
 
             return this.View(viewModel);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Create(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var category = this.Data.Categories.GetById(id);
+            if (category == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            var inputModel = new PostInputModel { CategoryId = category.Id, Category = category.Title };
+
+            return this.View(inputModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Title,Content,CategoryId,Category")] PostInputModel input)
+        {
+            if (input != null && this.ModelState.IsValid)
+            {
+                var userId = this.User.Identity.GetUserId();
+                var post = new Post
+                               {
+                                   Title = input.Title, 
+                                   Content = input.Content, 
+                                   AuthorId = userId, 
+                                   CategoryId = input.CategoryId
+                               };
+
+                this.Data.Posts.Add(post);
+                this.Data.SaveChanges();
+
+                return this.RedirectToAction("Details", "Posts", new { area = string.Empty, id = post.Id });
+            }
+
+            return this.View(input);
         }
     }
 }
