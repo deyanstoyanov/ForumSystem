e313359a0bfea9090289cebda8c0db05d18b32b3
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-19 07:18:13 +0300

    Created AnswerViewModel

---
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/Answer/AnswerViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace ForumSystem.Web.ViewModels.Answer
+{
+    using System;
+    using System.Collections.Generic;
+
+    using AutoMapper;
+
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+    using ForumSystem.Web.ViewModels.Comment;
+
+    public class AnswerViewModel : IMapFrom<Answer>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public int PostId { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public IEnumerable<CommentViewModel> Comments { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<Answer, AnswerViewModel>()
+                .ForMember(a => a.Author, config => config.MapFrom(a => a.Author.UserName));
+            configuration.CreateMap<Answer, AnswerViewModel>()
+                .ForMember(a => a.Comments, config => config.MapFrom(a => a.Comments));
+        }
+    }
+}
