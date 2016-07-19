a30f773d9acf1212f92b1d3205090abeaf4768d5
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-19 21:34:26 +0300

    Implemented SeedRoles

--- ForumSystem/Data/ForumSystem.Data/Migrations/Configuration.cs
+++ ForumSystem/Data/ForumSystem.Data/Migrations/Configuration.cs
@@ -1,6 +1,12 @@
 namespace ForumSystem.Data.Migrations
 {
     using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    using ForumSystem.Common.Constants;
+
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     public sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
@@ -12,6 +18,25 @@ namespace ForumSystem.Data.Migrations
 
         protected override void Seed(ApplicationDbContext context)
         {
+            this.SeedRoles(context);
+        }
+
+        private void SeedRoles(ApplicationDbContext context)
+        {
+            if (!context.Roles.Any())
+            {
+                var roleStore = new RoleStore<IdentityRole>(context);
+                var roleManager = new RoleManager<IdentityRole>(roleStore);
+                var roleNames = new[] { RoleConstants.Administrator, RoleConstants.Moderator };
+
+                foreach (var roleName in roleNames)
+                {
+                    var role = new IdentityRole { Name = roleName };
+                    roleManager.Create(role);
+                }
+
+                context.SaveChanges();
+            }
         }
     }
 }
