0d1101c9bbfb4d5e160eccb2472409bb973c22bb
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-19 14:22:09 +0300

    Created PostEditModel

---
+++ ForumSystem/Web/ForumSystem.Web/InputModels/Posts/PostEditModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ForumSystem.Web.InputModels.Posts
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class PostEditModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        [StringLength(200, MinimumLength = 7, ErrorMessage = "{0} must be between {2} and {1} symbols.")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Content")]
+        [StringLength(100000, MinimumLength = 12, ErrorMessage = "{0} must be between {2} and {1} symbols.")]
+        public string Content { get; set; }
+    }
+}
