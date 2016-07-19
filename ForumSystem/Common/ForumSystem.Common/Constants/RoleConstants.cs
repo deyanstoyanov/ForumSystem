1fef2100c1cb34bc61eabd55b7b7c0c6e2e7ebdc
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-19 21:11:49 +0300

    Created RoleConstants (Administrator, Moderator)

---
+++ ForumSystem/Common/ForumSystem.Common/Constants/RoleConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ForumSystem.Common.Constants
+{
+    public class RoleConstants
+    {
+        public const string Administrator = "Administrator";
+
+        public const string Moderator = "Moderator";
+    }
+}
