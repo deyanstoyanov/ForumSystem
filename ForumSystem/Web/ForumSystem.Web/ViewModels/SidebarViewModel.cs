94f5ff9ceeaa59c0ef4b5415b2fcb418396033d0
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-21 23:17:19 +0300

    Created SidebarViewModel

---
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/SidebarViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ForumSystem.Web.ViewModels
+{
+    using System.Collections.Generic;
+
+    using ForumSystem.Web.ViewModels.Category;
+    using ForumSystem.Web.ViewModels.Section;
+
+    public class SidebarViewModel
+    {
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
+
+        public IEnumerable<SectionViewModel> Sections { get; set; }
+    }
+}
