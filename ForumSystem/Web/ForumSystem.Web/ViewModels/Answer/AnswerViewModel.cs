61a400712b90397e6099c95f5fc561bc6e40318d
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-21 13:49:20 +0300

    UI improvements of Post details page

--- ForumSystem/Web/ForumSystem.Web/ViewModels/Answer/AnswerViewModel.cs
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/Answer/AnswerViewModel.cs
@@ -8,14 +8,17 @@
     using ForumSystem.Data.Models;
     using ForumSystem.Web.Infrastructure.Mapping;
     using ForumSystem.Web.ViewModels.Comment;
+    using ForumSystem.Web.ViewModels.Post;
 
-    public class AnswerViewModel : IMapFrom<Answer>, IHaveCustomMappings
+    public class AnswerViewModel : IMapFrom<Answer>, IHaveCustomMappings, IPostAnswer
     {
         public int Id { get; set; }
 
         public string Content { get; set; }
 
         public int PostId { get; set; }
+
+        public string AuthorId { get; set; }
 
         public string Author { get; set; }
 
--- ForumSystem/Web/ForumSystem.Web/ViewModels/Comment/CommentViewModel.cs
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/Comment/CommentViewModel.cs
@@ -6,14 +6,17 @@
 
     using ForumSystem.Data.Models;
     using ForumSystem.Web.Infrastructure.Mapping;
+    using ForumSystem.Web.ViewModels.Post;
 
-    public class CommentViewModel : IMapFrom<Comment>, IHaveCustomMappings
+    public class CommentViewModel : IMapFrom<Comment>, IHaveCustomMappings, IPostAnswer
     {
         public int Id { get; set; }
 
         public string Content { get; set; }
 
         public int AnswerId { get; set; }
+
+        public string AuthorId { get; set; }
 
         public string Author { get; set; }
 
