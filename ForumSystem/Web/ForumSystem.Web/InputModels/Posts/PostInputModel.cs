0762964a32bc4f1c3ff8976600354054efc99b0e
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-21 22:10:20 +0300

    Integrated TinyMCE editor

--- ForumSystem/Web/ForumSystem.Web/InputModels/Posts/PostInputModel.cs
+++ ForumSystem/Web/ForumSystem.Web/InputModels/Posts/PostInputModel.cs
@@ -1,15 +1,20 @@
 ﻿namespace ForumSystem.Web.InputModels.Posts
 {
     using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
 
     public class PostInputModel
     {
         [Required]
+        [AllowHtml]
         [Display(Name = "Title")]
         [StringLength(200, MinimumLength = 7, ErrorMessage = "{0} must be between {2} and {1} symbols.")]
         public string Title { get; set; }
 
         [Required]
+        [AllowHtml]
+        [DataType(DataType.Html)]
+        [UIHint("tinymce_full")]
         [Display(Name = "Content")]
         [StringLength(100000, MinimumLength = 12, ErrorMessage = "{0} must be between {2} and {1} symbols.")]
         public string Content { get; set; }
