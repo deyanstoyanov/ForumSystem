f607d4cc50598ca7ac7c4eb4bdd33c7860c3dec6
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-21 13:46:46 +0300

    Created _PostAnswerPartial view

---
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/Post/IPostAnswer.cs
@@ -0,0 +1,17 @@
+﻿namespace ForumSystem.Web.ViewModels.Post
+{
+    using System;
+
+    public interface IPostAnswer
+    {
+        int Id { get; }
+
+        string Content { get; }
+
+        string AuthorId { get; }
+
+        string Author { get; }
+
+        DateTime CreatedOn { get; }
+    }
+}
