d28a531bbd0f52f474fb6b2968976a8d45cc3417
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-26 12:51:28 +0300

    Created CommentsController

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/CommentsController.cs
@@ -0,0 +1,12 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+
+    public class CommentsController : BaseController
+    {
+        protected CommentsController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
