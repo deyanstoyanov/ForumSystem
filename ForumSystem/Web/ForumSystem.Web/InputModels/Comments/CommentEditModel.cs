21d6bee1bbab678c6a297e849795810c75b26e1b
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-26 20:55:31 +0300

    Created CommentEditModel

---
+++ ForumSystem/Web/ForumSystem.Web/InputModels/Comments/CommentEditModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ForumSystem.Web.InputModels.Comments
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    public class CommentEditModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [AllowHtml]
+        [DataType(DataType.Html)]
+        [UIHint("tinymce_full")]
+        [Display(Name = "Content")]
+        [StringLength(100000, MinimumLength = 12, ErrorMessage = "{0} must be between {2} and {1} symbols.")]
+        public string Content { get; set; }
+    }
+}
