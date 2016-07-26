4a6cb2d1f0b61befff74e29098f1cddefc09c2e5
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-26 16:44:28 +0300

    Created CommentInputModel

---
+++ ForumSystem/Web/ForumSystem.Web/InputModels/Comments/CommentInputModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ForumSystem.Web.InputModels.Comments
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    public class CommentInputModel
+    {
+        public int AnswerId { get; set; }
+
+        [Required]
+        [AllowHtml]
+        [DataType(DataType.Html)]
+        [UIHint("tinymce_full")]
+        [Display(Name = "Content")]
+        [StringLength(100000, MinimumLength = 12, ErrorMessage = "{0} must be between {2} and {1} symbols.")]
+        public string Content { get; set; }
+    }
+}
