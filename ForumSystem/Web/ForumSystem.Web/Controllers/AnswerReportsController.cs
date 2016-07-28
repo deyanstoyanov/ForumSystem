0bf0820c79f5327d1b1f82d8805b4d773efb1b4f
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-28 20:11:23 +0300

    Created AnswerReportsController

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/AnswerReportsController.cs
@@ -0,0 +1,15 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using ForumSystem.Data.UnitOfWork;
+
+    [Authorize]
+    public class AnswerReportsController : BaseController
+    {
+        public AnswerReportsController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
