a7318dc5af33c93cb6b52de9c432b7da018cb6d4
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-28 21:35:23 +0300

    Created CommentReportsController

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/CommentReportsController.cs
@@ -0,0 +1,15 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using ForumSystem.Data.UnitOfWork;
+
+    [Authorize]
+    public class CommentReportsController : BaseController
+    {
+        public CommentReportsController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
