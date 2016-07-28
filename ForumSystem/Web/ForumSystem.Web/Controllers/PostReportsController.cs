6013cbe9a719d274f920d3b19148197b99927bfc
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-28 16:57:37 +0300

    Created PostReportsController

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/PostReportsController.cs
@@ -0,0 +1,15 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using ForumSystem.Data.UnitOfWork;
+
+    [Authorize]
+    public class PostReportsController : BaseController
+    {
+        public PostReportsController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
