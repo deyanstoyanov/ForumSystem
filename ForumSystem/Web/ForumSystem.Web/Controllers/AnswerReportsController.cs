c85ab6feabf7cb218d5907ea9f2f7baa34e3754d
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-28 20:39:54 +0300

    Implemented creating a report for answer by authenticated user

--- ForumSystem/Web/ForumSystem.Web/Controllers/AnswerReportsController.cs
+++ ForumSystem/Web/ForumSystem.Web/Controllers/AnswerReportsController.cs
@@ -1,8 +1,13 @@
 ﻿namespace ForumSystem.Web.Controllers
 {
+    using System.Net;
     using System.Web.Mvc;
 
+    using ForumSystem.Data.Models;
     using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.InputModels.AnswerReports;
+
+    using Microsoft.AspNet.Identity;
 
     [Authorize]
     public class AnswerReportsController : BaseController
@@ -10,6 +15,53 @@
         public AnswerReportsController(IForumSystemData data)
             : base(data)
         {
+        }
+
+        [HttpGet]
+        public ActionResult Create(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var answer = this.Data.Answers.GetById(id);
+            if (answer == null || answer.IsDeleted)
+            {
+                return this.HttpNotFound();
+            }
+
+            var userid = this.User.Identity.GetUserId();
+            if (answer.AuthorId == userid)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var inputModel = new AnswerReportInputModel { AnswerId = answer.Id };
+
+            return this.PartialView(inputModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "AnswerId,Description")] AnswerReportInputModel input)
+        {
+            if (input != null && this.ModelState.IsValid)
+            {
+                var userId = this.User.Identity.GetUserId();
+                var report = new AnswerReport
+                                 {
+                                     AuthorId = userId, 
+                                     AnswerId = input.AnswerId, 
+                                     Description = input.Description
+                                 };
+
+                this.Data.AnswerReports.Add(report);
+                this.Data.SaveChanges();
+
+                return this.JsonSuccess("Successfully created report.");
+            }
+
+            return this.JsonError("Description is required");
         }
     }
 }
