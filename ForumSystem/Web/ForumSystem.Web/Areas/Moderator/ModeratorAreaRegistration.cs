07726c9f1fb4ec9db61d8bc4ba297e5ea6fc6e76
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-30 11:04:38 +0300

    Created Moderator area

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/ModeratorAreaRegistration.cs
@@ -0,0 +1,17 @@
+﻿namespace ForumSystem.Web.Areas.Moderator
+{
+    using System.Web.Mvc;
+
+    public class ModeratorAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "Moderator";
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Moderator_default", 
+                "Moderator/{controller}/{action}/{id}", 
+                new { action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
