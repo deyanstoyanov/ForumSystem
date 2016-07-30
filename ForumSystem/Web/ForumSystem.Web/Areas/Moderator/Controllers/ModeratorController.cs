7ff52f00b0949ddc325c1eb2fbef2f6461d1df44
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-30 11:14:32 +0300

    Created ModeratorController

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/ModeratorController.cs
@@ -0,0 +1,23 @@
+﻿namespace ForumSystem.Web.Areas.Moderator.Controllers
+{
+    using System.Web.Mvc;
+
+    using ForumSystem.Common.Constants;
+    using ForumSystem.Data.Models;
+    using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Controllers;
+
+    [Authorize(Roles = RoleConstants.Moderator)]
+    public class ModeratorController : BaseController
+    {
+        protected ModeratorController(IForumSystemData data)
+            : base(data)
+        {
+        }
+
+        protected ModeratorController(IForumSystemData data, ApplicationUser userProfile)
+            : base(data, userProfile)
+        {
+        }
+    }
+}
