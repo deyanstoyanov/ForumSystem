b9d92f9e98827ce9c95b2ccff1c3fb88df738f8b
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-31 21:44:48 +0300

    Created PostEditModel

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/InputModels/Posts/PostEditModel.cs
@@ -0,0 +1,26 @@
+﻿namespace ForumSystem.Web.Areas.Moderator.InputModels.Posts
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    public class PostEditModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 7, ErrorMessage = "{0} must be between {1} and {2} symbols.")]
+        public string Title { get; set; }
+
+        [Required]
+        [AllowHtml]
+        [DataType(DataType.Html)]
+        [UIHint("tinymce_full")]
+        [StringLength(100000, MinimumLength = 12, ErrorMessage = "{0} must be between {1} and {2} symbols.")]
+        public string Content { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+    }
+}
