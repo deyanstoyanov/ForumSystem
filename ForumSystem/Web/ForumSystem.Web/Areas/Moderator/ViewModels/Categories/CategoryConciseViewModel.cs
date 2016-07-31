a315d9d09eed41367e7f5b01bbeb26cebf33fee3
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-31 21:43:10 +0300

    Created CategoryConciseViewModel

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/ViewModels/Categories/CategoryConciseViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ForumSystem.Web.Areas.Moderator.ViewModels.Categories
+{
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+
+    public class CategoryConciseViewModel : IMapFrom<Category>
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
