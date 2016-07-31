6524c00845450811fcbfefc12f013bb88bb9497d
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-31 21:40:23 +0300

    Created PostsController

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/PostsController.cs
@@ -0,0 +1,12 @@
+﻿namespace ForumSystem.Web.Areas.Moderator.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+
+    public class PostsController : ModeratorController
+    {
+        public PostsController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
