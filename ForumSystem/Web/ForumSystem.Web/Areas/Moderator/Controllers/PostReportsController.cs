d18448552d6fc971cd966fbc96af3cf407a40cbd
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-02 10:09:56 +0300

    Created PostReportsController

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/PostReportsController.cs
@@ -0,0 +1,12 @@
+﻿namespace ForumSystem.Web.Areas.Moderator.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+
+    public class PostReportsController : ModeratorController
+    {
+        public PostReportsController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
