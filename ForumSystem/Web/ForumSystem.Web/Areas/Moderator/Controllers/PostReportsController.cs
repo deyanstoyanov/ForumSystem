1c69a75e109c1cf31d41677afc76ee1d7fbd5123
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-02 10:21:44 +0300

    Implemented displaying of all post reports in moderator area

--- ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/PostReportsController.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/PostReportsController.cs
@@ -1,12 +1,25 @@
 ﻿namespace ForumSystem.Web.Areas.Moderator.Controllers
 {
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
     using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Areas.Moderator.ViewModels.PostReports;
 
     public class PostReportsController : ModeratorController
     {
         public PostReportsController(IForumSystemData data)
             : base(data)
         {
+        }
+
+        [HttpGet]
+        public ActionResult All()
+        {
+            var reports = this.Data.PostReports.All().ProjectTo<PostReportViewModel>();
+
+            return this.View(reports);
         }
     }
 }
