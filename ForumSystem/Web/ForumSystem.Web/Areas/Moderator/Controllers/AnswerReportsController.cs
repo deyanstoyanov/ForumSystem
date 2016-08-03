1d1649039be1c27ee42e6d2af75488ea4513ae64
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-03 14:22:48 +0300

    Implemented deleting answer report by moderator

--- ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/AnswerReportsController.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/AnswerReportsController.cs
@@ -1,5 +1,6 @@
 ﻿namespace ForumSystem.Web.Areas.Moderator.Controllers
 {
+    using System.Net;
     using System.Web.Mvc;
 
     using AutoMapper.QueryableExtensions;
@@ -20,6 +21,25 @@
             var reports = this.Data.AnswerReports.All().ProjectTo<AnswerReportViewModel>();
 
             return this.View(reports);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var report = this.Data.AnswerReports.GetById(id);
+            if (report == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            this.Data.AnswerReports.Delete(id);
+            this.Data.SaveChanges();
+
+            return this.RedirectToAction("All");
         }
     }
 }
