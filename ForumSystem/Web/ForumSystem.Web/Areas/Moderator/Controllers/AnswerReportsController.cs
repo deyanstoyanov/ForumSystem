a304a6af7c381ee838ab2f9c8f03de72f8168cf1
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-03 10:06:59 +0300

    Created AnswerReportsController

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/AnswerReportsController.cs
@@ -0,0 +1,12 @@
+﻿namespace ForumSystem.Web.Areas.Moderator.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+
+    public class AnswerReportsController : ModeratorController
+    {
+        public AnswerReportsController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
