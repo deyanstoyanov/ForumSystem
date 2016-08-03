276d2795e033fb1b23743795e37d46cdb5271b4b
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-03 10:15:02 +0300

    Created AnswerReportViewModel

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/ViewModels/AnswerReports/AnswerReportViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace ForumSystem.Web.Areas.Moderator.ViewModels.AnswerReports
+{
+    using System;
+
+    using AutoMapper;
+
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+
+    public class AnswerReportViewModel : IMapFrom<AnswerReport>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string Author { get; set; }
+
+        public int AnswerId { get; set; }
+
+        public string Post { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<AnswerReport, AnswerReportViewModel>()
+                .ForMember(a => a.Author, config => config.MapFrom(a => a.Author.UserName));
+            configuration.CreateMap<AnswerReport, AnswerReportViewModel>()
+                .ForMember(a => a.Post, config => config.MapFrom(a => a.Answer.Post.Title));
+        }
+    }
+}
