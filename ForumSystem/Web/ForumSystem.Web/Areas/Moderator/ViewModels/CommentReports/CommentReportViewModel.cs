1b336b3d62079adbd06af2ed159fd82365df1d63
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-03 17:09:43 +0300

    Created CommentReportViewModel

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/ViewModels/CommentReports/CommentReportViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace ForumSystem.Web.Areas.Moderator.ViewModels.CommentReports
+{
+    using System;
+
+    using AutoMapper;
+
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+
+    public class CommentReportViewModel : IMapFrom<CommentReport>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string Author { get; set; }
+
+        public int PostId { get; set; }
+
+        public string Post { get; set; }
+
+        public int CommentId { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<CommentReport, CommentReportViewModel>()
+                .ForMember(c => c.Author, config => config.MapFrom(c => c.Author.UserName));
+            configuration.CreateMap<CommentReport, CommentReportViewModel>()
+                .ForMember(c => c.Post, config => config.MapFrom(c => c.Comment.Answer.Post.Title));
+        }
+    }
+}
