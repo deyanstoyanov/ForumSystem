3dc2a2e83f9c56111186e2fd52422403bcaf3cdd
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-03 10:02:48 +0300

    Implemented deleting a post report by moderator

--- ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/PostReportsController.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/PostReportsController.cs
@@ -1,5 +1,6 @@
 ﻿namespace ForumSystem.Web.Areas.Moderator.Controllers
 {
+    using System.Net;
     using System.Web.Mvc;
 
     using AutoMapper.QueryableExtensions;
@@ -20,6 +21,25 @@
             var reports = this.Data.PostReports.All().ProjectTo<PostReportViewModel>();
 
             return this.View(reports);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var report = this.Data.PostReports.GetById(id);
+            if (report == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            this.Data.PostReports.Delete(id);
+            this.Data.SaveChanges();
+
+            return this.RedirectToAction("All");
         }
     }
 }
