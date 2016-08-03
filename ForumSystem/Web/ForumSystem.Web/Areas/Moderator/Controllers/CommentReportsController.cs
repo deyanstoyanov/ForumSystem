a0d38f65911441d67b924c7d7b6063c0777cec45
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-03 17:18:09 +0300

    Implemented displaying of all comment reports in moderator area

--- ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/CommentReportsController.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/CommentReportsController.cs
@@ -1,12 +1,24 @@
 ﻿namespace ForumSystem.Web.Areas.Moderator.Controllers
 {
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
     using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Areas.Moderator.ViewModels.CommentReports;
 
     public class CommentReportsController : ModeratorController
     {
         public CommentReportsController(IForumSystemData data)
             : base(data)
         {
+        }
+
+        public ActionResult All()
+        {
+            var reports = this.Data.CommentReports.All().ProjectTo<CommentReportViewModel>();
+
+            return this.View(reports);
         }
     }
 }
