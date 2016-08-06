ecebba1805f600bc76e998ceb37a5d9c70728ede
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-06 20:49:45 +0300

    Created CategoryInputModel

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/InputModels/Categories/CategoryInputModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ForumSystem.Web.Areas.Administration.InputModels.Categories
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    public class CategoryInputModel
+    {
+        [Required]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "{0} must be between {1} and {2} symbols.")]
+        public string Title { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        public int SectionId { get; set; }
+
+        public IEnumerable<SelectListItem> Sections { get; set; }
+    }
+}
