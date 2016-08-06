402a7d99e39401cfa4897222dca5dc635f5ae641
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-06 20:38:17 +0300

    Implemented displaying information about all categories in administration area

--- ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/CategoriesController.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/CategoriesController.cs
@@ -1,12 +1,29 @@
 ﻿namespace ForumSystem.Web.Areas.Administration.Controllers
 {
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
     using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Areas.Administration.ViewModels.Categories;
 
     public class CategoriesController : AdministrationController
     {
         public CategoriesController(IForumSystemData data)
             : base(data)
         {
+        }
+
+        [HttpGet]
+        public ActionResult All()
+        {
+            var categories =
+                this.Data.Categories.AllWithDeleted()
+                    .OrderByDescending(c => c.CreatedOn)
+                    .ProjectTo<CategoryViewModel>();
+
+            return this.View(categories);
         }
     }
 }
