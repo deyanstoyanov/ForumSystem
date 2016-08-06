d326cc9056a1aa338c5115edd092708d2ccefc24
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-06 20:56:52 +0300

    Created SectionConciseViewModel

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/ViewModels/Sections/SectionConciseViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ForumSystem.Web.Areas.Administration.ViewModels.Sections
+{
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+
+    public class SectionConciseViewModel : IMapFrom<Section>
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
