9d423fb4ee6423255bc7ede6c75873b1802db9af
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-07 20:11:28 +0300

    Created SectionsController

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/SectionsController.cs
@@ -0,0 +1,12 @@
+﻿namespace ForumSystem.Web.Areas.Administration.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+
+    public class SectionsController : AdministrationController
+    {
+        public SectionsController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
