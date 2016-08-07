2e2aab72aea4d6976b85a3c076908978780b4aeb
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-07 20:34:11 +0300

    Implemented displaying information about all sections in administration area

--- ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/SectionsController.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/SectionsController.cs
@@ -1,12 +1,29 @@
 ﻿namespace ForumSystem.Web.Areas.Administration.Controllers
 {
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
     using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Areas.Administration.ViewModels.Sections;
 
     public class SectionsController : AdministrationController
     {
         public SectionsController(IForumSystemData data)
             : base(data)
         {
+        }
+
+        [HttpGet]
+        public ActionResult All()
+        {
+            var sections =
+                this.Data.Sections.AllWithDeleted()
+                    .OrderByDescending(s => s.CreatedOn)
+                    .ProjectTo<SectionViewModel>();
+
+            return this.View(sections);
         }
     }
 }
