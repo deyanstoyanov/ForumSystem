c88938308b2aebc0e453549b308bd83548dcbde0
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-07 20:40:44 +0300

    Created SectionInputModel

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/InputModels/Sections/SectionInputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ForumSystem.Web.Areas.Administration.InputModels.Sections
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class SectionInputModel
+    {
+        [Required]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "{0} must be between {1} and {2} symbols.")]
+        public string Title { get; set; }
+    }
+}
