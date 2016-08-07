d0e7448011d1f019e17038cdcefa4a4591fcfc5c
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-07 08:51:35 +0300

    Added AuthorizationRoles Attribute

---
+++ ForumSystem/Web/ForumSystem.Web.Infrastructure/Attributes/AuthorizeRolesAttribute.cs
@@ -0,0 +1,32 @@
+﻿namespace ForumSystem.Web.Infrastructure.Attributes
+{
+    using System;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using ForumSystem.Web.Infrastructure.Extensions;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class AuthorizeRolesAttribute : AuthorizeAttribute
+    {
+        private readonly string[] roles;
+
+        public AuthorizeRolesAttribute(params string[] roles)
+        {
+            this.roles = roles;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext == null)
+            {
+                throw new ArgumentNullException(nameof(httpContext));
+            }
+
+            var user = httpContext.User;
+
+            return user.IsLoggedIn() && this.roles.Any(x => user.IsInRole(x));
+        }
+    }
+}
