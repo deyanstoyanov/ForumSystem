169036cc2c7c6bae0356d58598d922424af73703
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-07 20:48:55 +0300

    Implemented creating new section by administrator

--- ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/SectionsController.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/SectionsController.cs
@@ -5,7 +5,9 @@
 
     using AutoMapper.QueryableExtensions;
 
+    using ForumSystem.Data.Models;
     using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Areas.Administration.InputModels.Sections;
     using ForumSystem.Web.Areas.Administration.ViewModels.Sections;
 
     public class SectionsController : AdministrationController
@@ -24,6 +26,29 @@
                     .ProjectTo<SectionViewModel>();
 
             return this.View(sections);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SectionInputModel input)
+        {
+            if (input != null && this.ModelState.IsValid)
+            {
+                var section = new Section { Title = input.Title };
+
+                this.Data.Sections.Add(section);
+                this.Data.SaveChanges();
+
+                return this.RedirectToAction("All");
+            }
+
+            return this.View(input);
         }
     }
 }
