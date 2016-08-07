ff0cf0237c44a7fcd92fe6d3f901b5de35f50a84
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-07 20:55:27 +0300

    Created SectionEditModel

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/InputModels/Sections/SectionEditModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ForumSystem.Web.Areas.Administration.InputModels.Sections
+{
+    public class SectionEditModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
