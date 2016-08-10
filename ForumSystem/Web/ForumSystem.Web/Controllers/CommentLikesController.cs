8b21d25eef70d53bb12c6520fd9ba657e67dfc4b
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-10 22:16:40 +0300

    Created CommentLikesController

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/CommentLikesController.cs
@@ -0,0 +1,15 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using ForumSystem.Data.UnitOfWork;
+
+    [Authorize]
+    public class CommentLikesController : BaseController
+    {
+        public CommentLikesController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
