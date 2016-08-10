4ebb3bb3d08159a0c7f9f305f36b02824c01f4db
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-10 19:50:28 +0300

    Created PostLikesController

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/PostLikesController.cs
@@ -0,0 +1,15 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using ForumSystem.Data.UnitOfWork;
+
+    [Authorize]
+    public class PostLikesController : BaseController
+    {
+        public PostLikesController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
