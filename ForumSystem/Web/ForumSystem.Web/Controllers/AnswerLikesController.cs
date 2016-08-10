4d6b5a14932a42c8a0bc7d884bcd0ecdb7214ec2
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-10 21:48:14 +0300

    Created AnswerLikesController

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/AnswerLikesController.cs
@@ -0,0 +1,15 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using ForumSystem.Data.UnitOfWork;
+
+    [Authorize]
+    public class AnswerLikesController : BaseController
+    {
+        public AnswerLikesController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
