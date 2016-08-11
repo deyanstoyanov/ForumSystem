771a4b6b02483fd467e31505b056fc3530af4c63
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-11 17:52:20 +0300

    Added PostLastActivityViewModel

---
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/LastActivities/PostLastActivityViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ForumSystem.Web.ViewModels.LastActivities
+{
+    using System;
+
+    public class PostLastActivityViewModel
+    {
+        public int AnswerId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
