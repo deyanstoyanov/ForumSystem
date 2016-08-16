498648d891f8e9d025515e97337d52afcc1bdf18
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-16 20:47:21 +0300

    Implemented displaying information about all comments in administration area

--- ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/CommentsController.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/CommentsController.cs
@@ -1,13 +1,29 @@
 ﻿namespace ForumSystem.Web.Areas.Administration.Controllers
 {
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
     using ForumSystem.Data.UnitOfWork;
     using ForumSystem.Web.Areas.Administration.Controllers.Base;
+    using ForumSystem.Web.Areas.Administration.ViewModels.Comments;
 
     public class CommentsController : AdministrationController
     {
         public CommentsController(IForumSystemData data)
             : base(data)
         {
+        }
+
+        public ActionResult All()
+        {
+            var comments =
+                this.Data.Comments.AllWithDeleted()
+                    .OrderByDescending(c => c.CreatedOn)
+                    .ProjectTo<CommentViewModel>();
+
+            return this.View(comments);
         }
     }
 }
--- ForumSystem/Web/ForumSystem.Web/Areas/Administration/ViewModels/Comments/CommentViewModel.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/ViewModels/Comments/CommentViewModel.cs
@@ -14,6 +14,8 @@
 
         public int AnswerId { get; set; }
 
+        public string Post { get; set; }
+
         public string AuthorId { get; set; }
 
         public string Author { get; set; }
@@ -38,6 +40,8 @@
                 .ForMember(c => c.LikesCount, config => config.MapFrom(c => c.Likes.Count(l => !l.IsDeleted)));
             configuration.CreateMap<Comment, CommentViewModel>()
                 .ForMember(c => c.ReportsCount, config => config.MapFrom(c => c.Reports.Count(r => !r.IsDeleted)));
+            configuration.CreateMap<Comment, CommentViewModel>()
+                .ForMember(c => c.Post, config => config.MapFrom(c => c.Answer.Post.Title));
         }
     }
 }
