9788e03bc6e54718aeaa3e61b465d9396d96fcf4
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-16 11:09:24 +0300

    Implemented displaying information about all posts in administration area

--- ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/PostsController.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/PostsController.cs
@@ -1,13 +1,27 @@
 ﻿namespace ForumSystem.Web.Areas.Administration.Controllers
 {
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
     using ForumSystem.Data.UnitOfWork;
     using ForumSystem.Web.Areas.Administration.Controllers.Base;
+    using ForumSystem.Web.Areas.Administration.ViewModels.Posts;
 
     public class PostsController : AdministrationController
     {
         public PostsController(IForumSystemData data)
             : base(data)
         {
+        }
+
+        [HttpGet]
+        public ActionResult All()
+        {
+            var posts = this.Data.Posts.AllWithDeleted().OrderByDescending(p => p.CreatedOn).ProjectTo<PostViewModel>();
+
+            return this.View(posts);
         }
     }
 }
