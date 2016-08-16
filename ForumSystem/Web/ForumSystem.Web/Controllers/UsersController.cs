990a89faa82008f5d3ca20d03035b4a105d178a3
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-16 22:22:06 +0300

    Created UsersController

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Controllers.Base;
+
+    public class UsersController : BaseController
+    {
+        public UsersController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
