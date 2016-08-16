06f759e791da623609d02c86105f4e3a996817fa
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-16 19:38:11 +0300

    Implemented editing a answer by administrator

--- ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/AnswersController.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/AnswersController.cs
@@ -9,6 +9,7 @@
 
     using ForumSystem.Data.UnitOfWork;
     using ForumSystem.Web.Areas.Administration.Controllers.Base;
+    using ForumSystem.Web.Areas.Administration.InputModels.Answers;
     using ForumSystem.Web.Areas.Administration.ViewModels.Answers;
 
     public class AnswersController : AdministrationController
@@ -27,6 +28,45 @@
                     .ProjectTo<AnswerViewModel>();
 
             return this.View(posts);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var answer = this.Data.Answers.GetById(id);
+            if (answer == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            var model = new AnswerEditModel { Id = answer.Id, Content = answer.Content, IsDeleted = answer.IsDeleted };
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(AnswerEditModel model)
+        {
+            if (model != null && this.ModelState.IsValid)
+            {
+                var answer = this.Data.Answers.GetById(model.Id);
+
+                answer.Content = model.Content;
+                answer.IsDeleted = model.IsDeleted;
+
+                this.Data.Answers.Update(answer);
+                this.Data.SaveChanges();
+
+                return this.RedirectToAction("All");
+            }
+
+            return this.View(model);
         }
 
         [HttpGet]
