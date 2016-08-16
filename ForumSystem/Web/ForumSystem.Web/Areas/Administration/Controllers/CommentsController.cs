0e226913777b8d2656baf71d5171bd258ab0c14c
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-16 19:42:00 +0300

    Created CommentsController in administration area

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/CommentsController.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumSystem.Web.Areas.Administration.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Areas.Administration.Controllers.Base;
+
+    public class CommentsController : AdministrationController
+    {
+        public CommentsController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
