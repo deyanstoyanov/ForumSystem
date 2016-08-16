54429b3ddce765e5dcf634929d4f4401ee668d73
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-16 10:13:09 +0300

    Created PostsController in administration area

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/PostsController.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumSystem.Web.Areas.Administration.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Areas.Administration.Controllers.Base;
+
+    public class PostsController : AdministrationController
+    {
+        public PostsController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
