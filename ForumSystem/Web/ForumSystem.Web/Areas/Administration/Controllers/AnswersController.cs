14c9be2ea504922e2864a45f6b197873578557dd
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-16 15:57:50 +0300

    Created AnswersController

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/AnswersController.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumSystem.Web.Areas.Administration.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Areas.Administration.Controllers.Base;
+
+    public class AnswersController : AdministrationController
+    {
+        public AnswersController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
