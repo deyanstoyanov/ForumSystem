5a7b4de115118e51140aba2bbabb77b2784bf98a
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-27 22:04:02 +0300

    Added CategoryLastPostViewModel

---
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/LastActivities/CategoryLastPostViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ForumSystem.Web.ViewModels.LastActivities
+{
+    using System;
+
+    public class CategoryLastPostViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
