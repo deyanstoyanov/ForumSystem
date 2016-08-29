2da19aeb206d7505832924bfb888cc59b66a4a46
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-29 14:41:31 +0300

    Implemented removing user from role by administrator
    Added RemoveUserFromRole view

--- ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/UsersController.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/UsersController.cs
@@ -27,10 +27,10 @@
         [HttpGet]
         public ActionResult All()
         {
-            var users =
+            var users = 
                 this.Data.Users.AllWithDeleted()
-                    .OrderByDescending(u => u.CreatedOn)
-                    .ProjectTo<UserViewModel>();
+                .OrderByDescending(u => u.CreatedOn)
+                .ProjectTo<UserViewModel>();
 
             return this.View(users);
         }
@@ -80,6 +80,60 @@
             {
                 var userManager = this.HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
                 userManager.AddToRole(input.UserId, input.RoleName);
+
+                this.Data.SaveChanges();
+
+                return this.RedirectToAction("All", "Users", new { area = "Administration" });
+            }
+
+            return this.View(input);
+        }
+
+        [HttpGet]
+        public ActionResult RemoveUserFromRole(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = this.Data.Users.GetById(id);
+            if (user == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            var identityStore = new IdentityDbContext();
+            var roles = identityStore.Roles.Select(r => r.Name).ToList();
+            var userManager = this.HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var availableUserRoles = new List<SelectListItem>();
+
+            foreach (var role in roles)
+            {
+                if (userManager.IsInRole(user.Id, role))
+                {
+                    availableUserRoles.Add(new SelectListItem { Value = role, Text = role });
+                }
+            }
+
+            var model = new AddUserInRoleInputModel
+                            {
+                                UserName = user.UserName, 
+                                UserId = user.Id, 
+                                Roles = availableUserRoles
+                            };
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RemoveUserFromRole(AddUserInRoleInputModel input)
+        {
+            if (input != null && this.ModelState.IsValid)
+            {
+                var userManager = this.HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                userManager.RemoveFromRole(input.UserId, input.RoleName);
 
                 this.Data.SaveChanges();
 
