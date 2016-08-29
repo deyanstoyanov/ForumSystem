7cbfd4035c9369adf82b811113e24617f95ae55d
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-08-29 14:13:06 +0300

    Implemented adding user in role by administrator
    Diesplayed user roles in administration area
    Added link to user profile page (Profile button)
    Removed unnecessary user information
    Added AddUserInRoleInputModel
    Added AddUserInRole view

--- ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/UsersController.cs
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/Controllers/UsersController.cs
@@ -1,13 +1,21 @@
 ﻿namespace ForumSystem.Web.Areas.Administration.Controllers
 {
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Net;
+    using System.Web;
     using System.Web.Mvc;
 
     using AutoMapper.QueryableExtensions;
 
     using ForumSystem.Data.UnitOfWork;
     using ForumSystem.Web.Areas.Administration.Controllers.Base;
+    using ForumSystem.Web.Areas.Administration.InputModels.Users;
     using ForumSystem.Web.Areas.Administration.ViewModels.Users;
+
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity.Owin;
 
     public class UsersController : AdministrationController
     {
@@ -25,6 +33,60 @@
                     .ProjectTo<UserViewModel>();
 
             return this.View(users);
+        }
+
+        [HttpGet]
+        public ActionResult AddUserInRole(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = this.Data.Users.GetById(id);
+            if (user == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            var identityStore = new IdentityDbContext();
+            var roles = identityStore.Roles.Select(r => r.Name).ToList();
+            var userManager = this.HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var availableUserRoles = new List<SelectListItem>();
+
+            foreach (var role in roles)
+            {
+                if (!userManager.IsInRole(user.Id, role))
+                {
+                    availableUserRoles.Add(new SelectListItem { Value = role, Text = role });
+                }
+            }
+
+            var model = new AddUserInRoleInputModel
+                            {
+                                UserName = user.UserName, 
+                                UserId = user.Id, 
+                                Roles = availableUserRoles
+                            };
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddUserInRole(AddUserInRoleInputModel input)
+        {
+            if (input != null && this.ModelState.IsValid)
+            {
+                var userManager = this.HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                userManager.AddToRole(input.UserId, input.RoleName);
+
+                this.Data.SaveChanges();
+
+                return this.RedirectToAction("All", "Users", new { area = "Administration" });
+            }
+
+            return this.View(input);
         }
     }
 }
---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Administration/InputModels/Users/AddUserInRoleInputModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ForumSystem.Web.Areas.Administration.InputModels.Users
+{
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    public class AddUserInRoleInputModel
+    {
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string RoleName { get; set; }
+
+        public IEnumerable<SelectListItem> Roles { get; set; }
+    }
+}
