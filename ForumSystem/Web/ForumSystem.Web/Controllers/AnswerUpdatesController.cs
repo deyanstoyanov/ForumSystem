e1c093a87fe8c9c2c27cfe2771f67f9706ae3816
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-09-01 23:45:27 +0300

    Implemented displaying last answer update
    Fixed references errors
    Added AnswerUpdatesController
    Added AnswerUpdateViewModel
    Added GetById view

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/AnswerUpdatesController.cs
@@ -0,0 +1,49 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using System.Linq;
+    using System.Net;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
+    using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Controllers.Base;
+    using ForumSystem.Web.ViewModels.AnswerUpdates;
+
+    public class AnswerUpdatesController : BaseController
+    {
+        public AnswerUpdatesController(IForumSystemData data)
+            : base(data)
+        {
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetById(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var answer = this.Data.Answers.GetById(id);
+            if (answer == null || answer.IsDeleted)
+            {
+                return this.HttpNotFound();
+            }
+
+            var model =
+                this.Data.AnswerUpdates.All()
+                    .Where(u => u.AnswerId == id)
+                    .OrderByDescending(u => u.CreatedOn)
+                    .ProjectTo<AnswerUpdateViewModel>()
+                    .FirstOrDefault();
+
+            if (model == null)
+            {
+                return new EmptyResult();
+            }
+
+            return this.PartialView(model);
+        }
+    }
+}
---
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/AnswerUpdates/AnswerUpdateViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace ForumSystem.Web.ViewModels.AnswerUpdates
+{
+    using AutoMapper;
+
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+
+    public class AnswerUpdateViewModel : IMapFrom<AnswerUpdate>, IHaveCustomMappings
+    {
+        public int AnswerId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string Author { get; set; }
+
+        public string Comment { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<AnswerUpdate, AnswerUpdateViewModel>()
+                .ForMember(u => u.Author, config => config.MapFrom(u => u.Author.UserName));
+        }
+    }
+}
