7eafc737701458a8d1164465145f98e45cce5b87
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-09-02 02:12:37 +0300

    Added CommentsController in moderator area

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/CommentsController.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumSystem.Web.Areas.Moderator.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Areas.Moderator.Controllers.Base;
+
+    public class CommentsController : ModeratorController
+    {
+        public CommentsController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
