10bceb4c65ac930040ad47955298d8de2095c890
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-09-01 11:54:04 +0300

    Implemented displaying last post update
    UI improvements on post details page
    Added PostUpdateViewModel
    Added GetById view

--- ForumSystem/Web/ForumSystem.Web/Controllers/PostUpdatesController.cs
+++ ForumSystem/Web/ForumSystem.Web/Controllers/PostUpdatesController.cs
@@ -1,13 +1,49 @@
 ﻿namespace ForumSystem.Web.Controllers
 {
+    using System.Linq;
+    using System.Net;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
     using ForumSystem.Data.UnitOfWork;
     using ForumSystem.Web.Controllers.Base;
+    using ForumSystem.Web.ViewModels.PostUpdates;
 
     public class PostUpdatesController : BaseController
     {
         public PostUpdatesController(IForumSystemData data)
             : base(data)
         {
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetById(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var post = this.Data.Posts.GetById(id);
+            if (post == null || post.IsDeleted)
+            {
+                return this.HttpNotFound();
+            }
+
+            var model =
+                this.Data.PostUpdates.All()
+                    .Where(u => u.PostId == id)
+                    .OrderByDescending(u => u.CreatedOn)
+                    .ProjectTo<PostUpdateViewModel>()
+                    .FirstOrDefault();
+
+            if (model == null)
+            {
+                return new EmptyResult();
+            }
+
+            return this.PartialView(model);
         }
     }
 }
---
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/PostUpdates/PostUpdateViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace ForumSystem.Web.ViewModels.PostUpdates
+{
+    using AutoMapper;
+
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+
+    public class PostUpdateViewModel : IMapFrom<PostUpdate>, IHaveCustomMappings
+    {
+        public int PostId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string Author { get; set; }
+
+        public string Comment { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<PostUpdate, PostUpdateViewModel>()
+                .ForMember(u => u.Author, config => config.MapFrom(u => u.Author.UserName));
+        }
+    }
+}
