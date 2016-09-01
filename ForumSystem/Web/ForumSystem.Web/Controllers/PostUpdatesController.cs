886c589d9212444af11db02d5d4d300f515da158
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-09-01 11:43:29 +0300

    Added PostUpdatesController

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/PostUpdatesController.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Controllers.Base;
+
+    public class PostUpdatesController : BaseController
+    {
+        public PostUpdatesController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
