4b141cc34758f4c624f60184bd79dc9c6f08bd3d
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-09-01 23:23:42 +0300

    Added AnswersController in moderator area

---
+++ ForumSystem/Web/ForumSystem.Web/Areas/Moderator/Controllers/AnswersController.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumSystem.Web.Areas.Moderator.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Areas.Moderator.Controllers.Base;
+
+    public class AnswersController : ModeratorController
+    {
+        public AnswersController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
