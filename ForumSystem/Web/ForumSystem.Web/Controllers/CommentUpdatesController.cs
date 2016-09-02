dae0de55b4cf974de534d40f75c8b33c03b7a520
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-09-02 10:54:25 +0300

    Added CommentUpdatesController

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/CommentUpdatesController.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Controllers.Base;
+
+    public class CommentUpdatesController : BaseController
+    {
+        public CommentUpdatesController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
