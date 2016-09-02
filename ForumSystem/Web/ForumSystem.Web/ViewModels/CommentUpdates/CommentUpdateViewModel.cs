5bddf3e60298c7212ffae6c83772ba4a6b0740a6
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-09-02 11:20:57 +0300

    Implemented displaying last comment update
    Added CommentUpdateViewModel and GetById view

---
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/CommentUpdates/CommentUpdateViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace ForumSystem.Web.ViewModels.CommentUpdates
+{
+    using AutoMapper;
+
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+
+    public class CommentUpdateViewModel : IMapFrom<CommentUpdate>, IHaveCustomMappings
+    {
+        public int CommentId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string Author { get; set; }
+
+        public string Reason { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<CommentUpdate, CommentUpdateViewModel>()
+                .ForMember(u => u.Author, config => config.MapFrom(u => u.Author.UserName));
+        }
+    }
+}
