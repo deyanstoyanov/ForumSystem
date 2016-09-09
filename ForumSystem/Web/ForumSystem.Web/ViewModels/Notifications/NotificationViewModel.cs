0f0825e9f6c67d65d7563eb1e05d28aec7c688ed
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-09-09 15:35:18 +0300

    Added NotificationViewModel

---
+++ ForumSystem/Web/ForumSystem.Web/ViewModels/Notifications/NotificationViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace ForumSystem.Web.ViewModels.Notifications
+{
+    using System;
+
+    using AutoMapper;
+
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+
+    public class NotificationViewModel : IMapFrom<Notification>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public int? ItemId { get; set; }
+
+        public string SenderId { get; set; }
+
+        public string Sender { get; set; }
+
+        public string ReceiverId { get; set; }
+
+        public string Receiver { get; set; }
+
+        public NotificationType NotificationType { get; set; }
+
+        public bool IsChecked { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<Notification, NotificationViewModel>()
+                .ForMember(n => n.Receiver, config => config.MapFrom(n => n.Receiver.UserName));
+            configuration.CreateMap<Notification, NotificationViewModel>()
+                .ForMember(n => n.Sender, config => config.MapFrom(n => n.Sender.UserName));
+        }
+    }
+}
