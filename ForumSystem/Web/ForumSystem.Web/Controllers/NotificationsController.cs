5202a9c59c3fdd99f8284282aee7d2ebe4e5e0eb
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-09-09 15:32:30 +0300

    Added NotificationsController

---
+++ ForumSystem/Web/ForumSystem.Web/Controllers/NotificationsController.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using ForumSystem.Data.UnitOfWork;
+    using ForumSystem.Web.Controllers.Base;
+
+    public class NotificationsController : BaseController
+    {
+        public NotificationsController(IForumSystemData data)
+            : base(data)
+        {
+        }
+    }
+}
