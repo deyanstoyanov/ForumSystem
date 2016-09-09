8d9279adf1e2a5898dcf8fd3bb1de805aeb9139b
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-09-09 15:58:14 +0300

    Added ForumSystemHub

---
+++ ForumSystem/Web/ForumSystem.Web/Hubs/ForumSystemHub.cs
@@ -0,0 +1,17 @@
+﻿namespace ForumSystem.Web.Hubs
+{
+    using System.Collections.Generic;
+
+    using ForumSystem.Data;
+    using ForumSystem.Data.UnitOfWork;
+
+    using Microsoft.AspNet.SignalR;
+    using Microsoft.AspNet.SignalR.Hubs;
+
+    [Authorize]
+    [HubName("forumSystemHub")]
+    public class ForumSystemHub : Hub
+    {
+        public static IForumSystemData Data => new ForumSystemData(new ApplicationDbContext());
+    }
+}
