035a9289acb800325d9f4c1f95cab8c006783a24
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-09-09 15:05:27 +0300

    Implemented SeedUsers

--- ForumSystem/Data/ForumSystem.Data/Migrations/Configuration.cs
+++ ForumSystem/Data/ForumSystem.Data/Migrations/Configuration.cs
@@ -1,9 +1,11 @@
 namespace ForumSystem.Data.Migrations
 {
+    using System;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
     using ForumSystem.Common.Constants;
+    using ForumSystem.Data.Models;
 
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
@@ -19,6 +21,7 @@ namespace ForumSystem.Data.Migrations
         protected override void Seed(ApplicationDbContext context)
         {
             this.SeedRoles(context);
+            this.SeedUsers(context);
         }
 
         private void SeedRoles(ApplicationDbContext context)
@@ -34,6 +37,34 @@ namespace ForumSystem.Data.Migrations
                     var role = new IdentityRole { Name = roleName };
                     roleManager.Create(role);
                 }
+
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedUsers(ApplicationDbContext context)
+        {
+            if (!context.Users.Any())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                var admin = new ApplicationUser
+                                {
+                                    UserName = "administrator", 
+                                    Email = "administrator@fake.com", 
+                                    CreatedOn = DateTime.Now
+                                };
+
+                var userCreateResult = userManager.Create(admin, "123456");
+                if (userCreateResult.Succeeded)
+                {
+                    context.Users.AddOrUpdate(admin);
+                }
+
+                context.SaveChanges();
+
+                userManager.AddToRole(admin.Id, RoleConstants.Administrator);
 
                 context.SaveChanges();
             }
